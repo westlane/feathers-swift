e29aa7474f38643c2f1785cb6eed71ce405f3b75
West Lane <westlane@users.noreply.github.com>
2026-05-25 01:50:38 -0400

    chore: add watchOS 11 and visionOS 2 platform targets

--- Package.swift
+++ Package.swift
@@ -6,7 +6,9 @@ let package = Package(
     name: "Feathers",
     platforms: [
         .iOS(.v18),
-        .macOS(.v15)
+        .macOS(.v15),
+        .watchOS(.v11),
+        .visionOS(.v2)
     ],
     products: [
         .library(
